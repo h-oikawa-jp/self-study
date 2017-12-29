fa45918ef248a302fe46c09e532ac5d8a8d5666e
oikawa <oikawa@techfun.jp>
2017-12-30 00:46:28 +0900

    Exercise 3.28

--- fpinscala/exercises/src/main/scala/fpinscala/datastructures/Tree.scala
+++ fpinscala/exercises/src/main/scala/fpinscala/datastructures/Tree.scala
@@ -21,4 +21,9 @@ object Tree {
     case Branch(l, r) => 1 + { depth(l) max depth(r) }
     case Leaf(_) => 0
   }
+
+  def map[A,B](t: Tree[A])(f: A => B): Tree[B] = t match {
+    case Leaf(a) => Leaf(f(a))
+    case Branch(l, r) => Branch(map(l)(f), map(r)(f))
+  }
 }

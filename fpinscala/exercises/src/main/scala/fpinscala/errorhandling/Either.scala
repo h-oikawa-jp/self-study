7bfa3a8f3bf4d160417f390bb5bfa1126a650335
oikawa <oikawa@techfun.jp>
2017-12-30 03:53:06 +0900

    Exercise 4.7

--- fpinscala/exercises/src/main/scala/fpinscala/errorhandling/Either.scala
+++ fpinscala/exercises/src/main/scala/fpinscala/errorhandling/Either.scala
@@ -28,9 +28,11 @@ case class Left[+E](get: E) extends Either[E,Nothing]
 case class Right[+A](get: A) extends Either[Nothing,A]
 
 object Either {
-  def traverse[E,A,B](es: List[A])(f: A => Either[E, B]): Either[E, List[B]] = ???
+  def traverse[E,A,B](es: List[A])(f: A => Either[E, B]): Either[E, List[B]] = es.foldRight[Either[E, List[B]]](Right(Nil)) {
+    (a, eb) => eb flatMap { l => f(a) map (_ :: l) }
+  }
 
-  def sequence[E,A](es: List[Either[E,A]]): Either[E,List[A]] = ???
+  def sequence[E,A](es: List[Either[E,A]]): Either[E,List[A]] = traverse(es)(identity)
 
   def mean(xs: IndexedSeq[Double]): Either[String, Double] = 
     if (xs.isEmpty) 
